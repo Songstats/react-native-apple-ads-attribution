82dd3df6e66f3a6d3a1b71812944a179e9a89889
Oskar Eichler <62393985+OskarEichler@users.noreply.github.com>
2025-12-27 10:26:10 +0700

    Refactor NativeAppleAdsAttribution module initialization

--- src/NativeAppleAdsAttribution.js
+++ src/NativeAppleAdsAttribution.js
@@ -1,7 +1,7 @@
 // @flow
 
 import type {TurboModule} from 'react-native';
-import {NativeModules, Platform, TurboModuleRegistry} from 'react-native';
+import {TurboModuleRegistry} from 'react-native';
 
 export interface Spec extends TurboModule {
   +getAttributionData: () => Promise<Object>;
@@ -9,20 +9,4 @@ export interface Spec extends TurboModule {
   +getAdServicesAttributionData: () => Promise<Object>;
 }
 
-const LINKING_ERROR =
-  `The package '@hexigames/react-native-apple-ads-attribution' doesn't seem to be linked. Make sure: \n\n` +
-  Platform.select({ios: "- You have run 'pod install'\n", default: ''}) +
-  '- You rebuilt the app after installing the package\n' +
-  '- You are not using Expo managed workflow\n';
-
-const isTurboModuleEnabled = global.__turboModuleProxy != null;
-
-const AppleAdsAttributionModule = isTurboModuleEnabled
-  ? TurboModuleRegistry.getEnforcing<Spec>('AppleAdsAttribution')
-  : NativeModules.AppleAdsAttribution;
-
-if (!AppleAdsAttributionModule && Platform.OS === 'ios') {
-  throw new Error(LINKING_ERROR);
-}
-
-export default ((AppleAdsAttributionModule || {}: any): Spec);
+export default TurboModuleRegistry.getEnforcing<Spec>('AppleAdsAttribution');

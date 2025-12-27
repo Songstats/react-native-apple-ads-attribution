a66247bd43c33d4fd04dbd4cd181231533290828
Oskar Eichler <62393985+OskarEichler@users.noreply.github.com>
2025-12-27 11:01:43 +0700

    Update AppleAdsAttribution methods for non-iOS support
    Reject promises with an error message for non-iOS platforms.

--- index.js
+++ index.js
@@ -4,21 +4,21 @@ import AppleAdsAttributionModule from './src/NativeAppleAdsAttribution';
 class AppleAdsAttribution {
   getAttributionData() {
     if (Platform.OS !== 'ios') {
-      return null;
+      return Promise.reject(new Error('AppleAdsAttribution is only available on iOS'));
     }
     return AppleAdsAttributionModule.getAttributionData();
   }
 
   getAdServicesAttributionToken() {
     if (Platform.OS !== 'ios') {
-      return null;
+      return Promise.reject(new Error('AppleAdsAttribution is only available on iOS'));
     }
     return AppleAdsAttributionModule.getAdServicesAttributionToken();
   }
 
   getAdServicesAttributionData() {
     if (Platform.OS !== 'ios') {
-      return null;
+      return Promise.reject(new Error('AppleAdsAttribution is only available on iOS'));
     }
     return AppleAdsAttributionModule.getAdServicesAttributionData();
   }
